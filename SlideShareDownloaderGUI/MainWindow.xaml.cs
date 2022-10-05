b2eed2c3e6777c8590b31faddcda509c2b4340d0
charg2dang <charg2dang@kakao.com>
2022-10-06 02:04:29 +0900

    feat: WPF GUI 프로젝트 추가

---
+++ SlideShareDownloaderGUI/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlideShareDownloaderGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string _url = string.Empty; //< url
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listBox.ItemsSource = new List< A >() 
+                {
+                    new (){ Name = "ABC", Progressed = 33, Max = 100 },
+                    new (){ Name = "CDE", Progressed = 33, Max = 100 },
+                    new (){ Name = "EFG", Progressed = 78, Max = 100 },
+                };
+        }
+
+        /// <summary>
+        /// 다운로드 버튼 클릭시 호출
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_StartDownload( object sender, RoutedEventArgs e )
+        {
+            if ( SlideShareDownloader.App.Instance.Download( _url ) )
+                ;
+        }
+
+        /// <summary>
+        /// 텍스트 박스의 텍스트가 변경시 호출
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_TextChanged( object sender, RoutedEventArgs e )
+        {
+            TextBox textBox = sender as TextBox;
+            _url = textBox.Text;
+        }
+
+        private void ListBox_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+        }
+
+        public class Slide
+        {
+            public string Name       { get; set; } = string.Empty;
+            public int    Downloaded { get; set; } = 0;
+        }
+
+        public class A
+        {
+            public string Name       { get; set; } = string.Empty;
+            public string Name2      { get; set; } = string.Empty;
+            public int    Progressed { get; set; } = 0;
+            public int    Max        { get; set; } = 100;
+        }
+
+    }
+}
